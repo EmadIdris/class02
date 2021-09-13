16f9c0e5a73e039042591741f1fca1eb7a97e674
EmadIdris <emadidris@outlook.com>
2021-09-13 14:30:12 +0300

    solve

---
+++ src/component/Foteer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Foteer extends React.Component{
+
+    render(){
+
+        return(
+
+        <h3> CopyRight Emad Idris</h3>
+
+        )
+
+        }
+
+    }
+
+
+
+export default  Foteer;
---
+++ src/component/HornedBeasts.js
@@ -0,0 +1,54 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+ class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfPets: 0
+        }
+    }
+
+    // for adding 1 to the clicked on cat
+    increaseNumberOfPets = () => {
+        this.setState({
+            numOfPets: this.state.numOfPets + 1
+        })
+    }
+    render() {
+
+        return (
+
+            <div> 
+{/* 
+                <h1>{this.props.title}</h1>
+                <p>{this.state.numOfPets}</p>
+                <img src={this.props.imgUrl} alt= {this.props.description}/>
+
+                <p>{this.props.description}</p> */}
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img className='cardCat' variant="top" src={this.props.imgUrl} alt= {this.props.description} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            Number of Pets {this.state.numOfPets}
+                        </Card.Text>
+                        <Button onClick={this.increaseNumberOfPets} variant="primary">Vote</Button>
+                    </Card.Body>
+                </Card>
+                
+
+            </div>
+
+        )
+
+    }
+
+}
+
+
+
+export default HornedBeasts;
