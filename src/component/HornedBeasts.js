c197144ee9ede997fa6580a51c8ea36421cfa7f5
EmadIdris <emadidris@outlook.com>
2021-09-14 16:56:33 +0300

    solve lab 03

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,49 +1,38 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
- class HornedBeasts extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numOfPets: 0
-        };
-    }
+class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
 
-    // for adding 1 to the clicked on cat
-    increaseNumberOfPets = () => {
-        this.setState({
-            numOfPets: this.state.numOfPets + 1
-        });
-    }
-    render() {
+    this.state = {
+      addHeart: 0,
+    };
+  }
+  buttonClicked = () => {
+  this.setState({addHeart: this.state.addHeart + 1});
+  this.props.showModal(this.props.title);
+}
+  render() {
+    
+    const image = this.props.img;
+    const title = this.props.title;
+    const description = this.props.description;
 
-        return (
-
-            <div> 
-
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className='cardCat' variant="top" src={this.props.imgUrl} alt= {this.props.description} />
-                    <Card.Body>
-                        <Card.Title>{this.props.title}</Card.Title>
-                        <Card.Text>
-                            Number of Pets {this.state.numOfPets}
-                        </Card.Text>
-                        <Button onClick={this.increaseNumberOfPets} variant="primary">Vote</Button>
-                    </Card.Body>
-                </Card>
-                
-
-            </div>
-
-        );
-
-    }
-
+    return (
+      <div>
+        <Card style={{ width: '18rem' }}
+          onClick={this.buttonClicked}>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{description}</Card.Text>
+              <Card.Text>❤️ = {this.state.addHeart}</Card.Text>
+            </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
-
-
-export default HornedBeasts;
+export default HornedBeast;
